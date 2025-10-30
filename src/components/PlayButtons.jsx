9aec03145481eab902f11c0d5f181822b2ceecf3
Yu Zhang <zhayy374@mymail.unisa.edu.au>
2025-10-30 19:49:58 +1030

    Change Stop button style to danger

--- src/components/PlayButtons.jsx
+++ src/components/PlayButtons.jsx
@@ -5,7 +5,7 @@ function PlayButtons() {
     <>
       <div className='btn-group' role='group' aria-label='Basic mixed styles example'>
           <button id="play" className="btn btn-outline-primary">Play</button>
-          <button id="stop" className="btn btn-outline-primary">Stop</button>
+          <button id="stop" className="btn btn-outline-danger">Stop</button>
       </div>     
     </>
   )
