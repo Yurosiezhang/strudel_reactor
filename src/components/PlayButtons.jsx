6115fbeddc5527dffa3308c42acebe5dfc5b5cf9
Yu Zhang <zhayy374@mymail.unisa.edu.au>
2025-10-30 18:53:45 +1030

    Fix the indentation

--- src/components/PlayButtons.jsx
+++ src/components/PlayButtons.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 function PlayButtons() {
   return (
     <>
-    <div className='btn-group' role='group' aria-label='Basic mixed styles example'>
-        <button id="play" className="btn btn-outline-primary">Play</button>
-        <button id="stop" className="btn btn-outline-primary">Stop</button>
-    </div>     
+      <div className='btn-group' role='group' aria-label='Basic mixed styles example'>
+          <button id="play" className="btn btn-outline-primary">Play</button>
+          <button id="stop" className="btn btn-outline-primary">Stop</button>
+      </div>     
     </>
   )
 }
--- src/components/ProcButtons.jsx
+++ src/components/ProcButtons.jsx
@@ -6,7 +6,7 @@ function ProcButtons() {
         <div className='btn-group' role='group' aria-label='Basic mixed styles example'>
             <button id="process" className="btn btn-outline-primary">Preprocess</button>
             <button id="process_play" className="btn btn-outline-primary">Proc & Play</button>
-    </div>
+        </div>
     </>                                    
   )
 }
