f3956faf25c1946cf06b9e75f84396712e2a7cdd
Yu Zhang <zhayy374@mymail.unisa.edu.au>
2025-10-31 23:02:24 +1030

    Refactor the DJControls component to add input, volume range slider and checkboxes

--- src/components/DJControls.jsx
+++ src/components/DJControls.jsx
@@ -3,18 +3,32 @@ import React from 'react'
 function DJControls() {
   return (
     <>
-        <div className="form-check">
-            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" defaultChecked />
-            <label className="form-check-label" htmlFor="flexRadioDefault1">
-                p1: ON
-            </label>
+        {/* input field */}
+        <div class="input-group mb-3">
+            <span class="input-group-text" id="cpm_label">setCPM</span>
+            <input type="text" className="form-control" id="cpm_text_input" placeholder="120" aria-label="cpm" aria-describedby="cpm_label" />
         </div>
+        
+        {/* Volume range slider */}
+        <label for="volume_range" className="form-label">Volume</label>
+        <input type="range" className="form-range"  min="0" max="1" step="0.01" id="volume_range" />
+
+        {/* Checkboxes */}
         <div className="form-check">
-            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"  />
-            <label className="form-check-label" htmlFor="flexRadioDefault2">
-                p1: HUSH
-            </label>
-        </div>      
+            <input className="form-check-input" type="checkbox" value="" id="s1" />
+            <label className="form-check-label" for="s1">s1</label>
+        </div>
+
+        <div className="form-check">
+            <input className="form-check-input" type="checkbox" value="" id="d1" />
+            <label className="form-check-label" for="d1">d1</label>
+        </div>
+
+        <div className="form-check">
+            <input className="form-check-input" type="checkbox" value="" id="d2" />
+            <label className="form-check-label" for="d2">d2</label>
+        </div>
+
     </>
   )
 }
