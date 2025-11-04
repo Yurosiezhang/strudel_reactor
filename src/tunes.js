3afb3b86d4e3b4aac5dac08959c7056947338807
Yu Zhang <zhayy374@mymail.unisa.edu.au>
2025-11-04 14:24:31 +1030

    Change the music to coastline

--- src/tunes.js
+++ src/tunes.js
@@ -1,100 +1,59 @@
-export const stranger_tune = `setcps(140/60/4)
+export const coastline_tune = `
 
-samples('github:algorave-dave/samples')
+samples('github:eddyflux/crate')
 samples('https://raw.githubusercontent.com/tidalcycles/Dirt-Samples/master/strudel.json')
 samples('https://raw.githubusercontent.com/Mittans/tidal-drum-machines/main/machines/tidal-drum-machines.json')
 
-const gain_patterns = [
-  "2",
-  "{0.75 2.5}*4",
-    "{0.75 2.5!9 0.75 2.5!5 0.75 2.5 0.75 2.5!7 0.75 2.5!3 <2.5 0.75> 2.5}%16",
-]
+setcps(0.75)
+let chords = chord("<Bbm9 Fm9>/4").dict('ireal')
 
-const drum_structure = [
-"~",
-"x*4",
-"{x ~!9 x ~!5 x ~ x ~!7 x ~!3 < ~ x > ~}%16",
-]
-
-const basslines = [
-  "[[eb1, eb2]!16 [f2, f1]!16 [g2, g1]!16 [f2, f1]!8 [bb2, bb1]!8]/8",
-  "[[eb1, eb2]!16 [bb2, bb1]!16 [g2, g1]!16 [f2, f1]!4 [bb1, bb2]!4 [eb1, eb2]!4 [f1, f2]!4]/8"
-]
-
-const arpeggiator1 = [
-"{d4 bb3 eb3 d3 bb2 eb2}%16",
-"{c4 bb3 f3 c3 bb2 f2}%16",
-"{d4 bb3 g3 d3 bb2 g2}%16",
-"{c4 bb3 f3 c3 bb2 f2}%16",
-]
-
-const arpeggiator2 = [
-"{d4 bb3 eb3 d3 bb2 eb2}%16",
-"{c4 bb3 f3 c3 bb2 f2}%16",
-"{d4 bb3 g3 d3 bb2 g2}%16",
-"{d5 bb4 g4 d4 bb3 g3 d4 bb3 eb3 d3 bb2 eb2}%16",
-]
-
-
-const pattern = 0
-const bass = 0
-
-bassline:
-note(pick(basslines, bass))
-.sound("supersaw")
-.postgain(2)
-.room(0.6)
-.lpf(700)
-.room(0.4)
-.postgain(pick(gain_patterns, pattern))
-
-
-main_arp: 
-note(pick(arpeggiator1, "<0 1 2 3>/2"))
-.sound("supersaw")
-.lpf(300)
-.adsr("0:0:.5:.1")
-.room(0.6)
-.lpenv(3.3)
-.postgain(pick(gain_patterns, pattern))
-
-
-drums:
 stack(
-  s("tech:5")
-  .postgain(6)
-  .pcurve(2)
-  .pdec(1)
-  .struct(pick(drum_structure, pattern)),
+  <track_drums>
+  stack(
+    s("bd").struct("<[x*<1 2> [~@3 x]] x>"),
+    s("~ [rim, sd:<2 3>]").room("<0 .2>"),
+    n("[0 <1 3>]*<2!3 4>").s("hh"),
+    s("rd:<1!3 2>*2").mask("<0 0 1 1>/16").gain(0.5)
+  )
+  .bank("crate")
+  .mask("<[0 1] 1 1 1>/16".early(0.5))
+  </track_drums>,
 
-  s("sh").struct("[x!3 ~!2 x!10 ~]")
-  .postgain(0.5).lpf(7000)
-  .bank("RolandTR808")
-  .speed(0.8).jux(rev).room(sine.range(0.1,0.4)).gain(0.6),
+  <track_chords>
+  chords.offset(-1)
+    .voicing()
+    .s("gm_epiano1:1")
+    .phaser(4)
+    .room(0.5)
+  </track_chords>,
 
-  s("{~ ~ rim ~ cp ~ rim cp ~!2 rim ~ cp ~ < rim ~ >!2}%8 *2")
-  .bank("[KorgDDM110, OberheimDmx]").speed(1.2)
-  .postgain(.25),
+  <track_melody>
+  stack(
+    n("<0!3 1*2>")
+      .set(chords)
+      .mode("root:g2")
+      .voicing()
+      .s("gm_acoustic_bass"),
+
+    chords.n("[0 <4 3 <2 5>>*2](<3 5>,8)")
+      .anchor("D5")
+      .voicing()
+      .segment(4)
+      .clip(rand.range(0.4, 0.8))
+      .room(0.75)
+      .shape(0.3)
+      .delay(0.25)
+      .fm(sine.range(3,8).slow(8))
+      .lpf(sine.range(500,1000).slow(8))
+      .lpq(5)
+      .rarely(ply("2"))
+      .chunk(4, fast(2))
+      .gain(perlin.range(0.6, 0.9))
+      .mask("<0 1 1 0>/16")
+  )
+  </track_melody>
 )
-
-drums2: 
-stack(
-  s("[~ hh]*4").bank("RolandTR808").room(0.3).speed(0.75).gain(1.2),
-  s("hh").struct("x*16").bank("RolandTR808")
-  .gain(0.6)
-  .jux(rev)
-  .room(sine.range(0.1,0.4))
-  .postgain(0.5),
-  
-  s("[psr:[2|5|6|7|8|9|12|24|25]*16]?0.1")
-  .gain(0.1)
-  .postgain(pick(gain_patterns, pattern))
-  .hpf(1000)
-  .speed(0.5)
-  .rarely(jux(rev)),
-)
-//Remixed and reproduced from Algorave Dave's code found here: https://www.youtube.com/watch?v=ZCcpWzhekEY
-// all(x => x.gain(mouseX.range(0,1)))
-// all(x => x.log())
-
-// @version 1.2`;
+.late("[0 .01]*4")
+.late("[0 .01]*2")
+.size(4)
+`;
