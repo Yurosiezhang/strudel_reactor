61481e1f9a2512ad1ab81e508ac4278a04dd0e7c
Yu Zhang <zhayy374@mymail.unisa.edu.au>
2025-11-06 23:02:49 +1030

    Add a Graph placeholder

---
+++ src/components/Graph.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Graph() {
+  return (
+    <>
+        <div>
+            D3 Graph Placeholder
+        </div>
+
+      
+    </>
+  )
+}
+
+export default Graph
