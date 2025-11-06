5f2679ee622edd3dbba7a1b7572f1fb82fc4f7be
Yu Zhang <zhayy374@mymail.unisa.edu.au>
2025-11-06 23:02:13 +1030

    style: refine textarea component

--- src/components/PreprocessTextarea.jsx
+++ src/components/PreprocessTextarea.jsx
@@ -3,8 +3,13 @@ import React from 'react'
 function PreprocessTextarea({ defaultValue, onChange }) {
   return (
     <>
-        <label htmlFor="exampleFormControlTextarea1" className="form-label">Text to preprocess:</label>
-        <textarea className="form-control" rows="15" defaultValue={defaultValue} onChange={onChange} id="proc" ></textarea>     
+        <label htmlFor="exampleFormControlTextarea1" 
+          className="form-label fw-semibold text-light mb-2"
+          >Text to preprocess:</label>
+        <textarea className="form-control preprocess-textarea" rows="15" 
+        defaultValue={defaultValue} 
+        onChange={onChange} 
+        id="proc" ></textarea>     
     </>
   )
 }
