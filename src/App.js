5fc241681bfad45295b9f3158756d31f1efdafb5
Yu Zhang <zhayy374@mymail.unisa.edu.au>
2025-11-07 15:52:49 +1030

    Remove commented-out JS code for part A submission

--- src/App.js
+++ src/App.js
@@ -13,62 +13,11 @@ import banner from './images/banner.png';
 import SettingControls from './components/SettingControls';
 
 
-
-
-// let globalEditor = null;
-
 const handleD3Data = (event) => {
     console.log(event.detail);
 };
 
-// export function SetupButtons() {
-
-//     document.getElementById('play').addEventListener('click', () => globalEditor.evaluate());
-//     document.getElementById('stop').addEventListener('click', () => globalEditor.stop());
-//     document.getElementById('process').addEventListener('click', () => {
-//         Proc()
-//     }
-//     )
-//     document.getElementById('process_play').addEventListener('click', () => {
-//         if (globalEditor != null) {
-//             Proc()
-//             globalEditor.evaluate()
-//         }
-//     }
-//     )
-// }
-
-
-
-// export function ProcAndPlay() {
-//     if (globalEditor != null && globalEditor.repl.state.started == true) {
-//         console.log(globalEditor)
-//         Proc()
-//         globalEditor.evaluate();
-//     }
-// }
-
-// export function Proc() {
-
-//     let proc_text = document.getElementById('proc').value
-//     let proc_text_replaced = proc_text.replaceAll('<p1_Radio>', ProcessText);
-//     ProcessText(proc_text);
-//     globalEditor.setCode(proc_text_replaced)
-// }
-
-// export function ProcessText(match, ...args) {
-
-//     let replace = ""
-//     // if (document.getElementById('flexRadioDefault2').checked) {
-//     //     replace = "_"
-//     // }
-
-//     return replace
-// }
-
 export default function StrudelDemo() {
-
-    // const hasRun = useRef(false);
 
     const [songText, setSongText] = useState(coastline_tune)
     const [editor, setEditor] = useState()
@@ -118,31 +67,6 @@ export default function StrudelDemo() {
         handleProcess();
         editor?.evaluate();
     }
-
-
-
-// useEffect(() => {
-
-//     if (!hasRun.current) {
-//         // document.addEventListener("d3Data", handleD3Data);
-//         // console_monkey_patch();
-//         // hasRun.current = true;
-//         // //Code copied from example: https://codeberg.org/uzu/strudel/src/branch/main/examples/codemirror-repl
-//         //     //init canvas
-//         //     const canvas = document.getElementById('roll');
-//         //     canvas.width = canvas.width * 2;
-//         //     canvas.height = canvas.height * 2;
-//         //     const drawContext = canvas.getContext('2d');
-//         //     const drawTime = [-2, 2]; // time window of drawn haps
-
-            
-//         document.getElementById('proc').value = stranger_tune
-//         // SetupButtons()
-//         // Proc()
-//     }
-//     globalEditor.setCode(songText);
-
-// }, [songText]);
 
 
 return (
