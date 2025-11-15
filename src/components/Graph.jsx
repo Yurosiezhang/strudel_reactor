8827fe29f9fcfd5e9e61894d4014a45c6e0f3e39
Yu Zhang <zhayy374@mymail.unisa.edu.au>
2025-11-16 00:48:49 +1030

    Draw d3 graph

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -41,6 +41,79 @@ function Graph() {
     return 0;
   }
 
+  // Draw d3 graph
+  useEffect(()=>{
+
+    // select SVG element
+    const svg = d3.select('svg');
+    svg.selectAll("*").remove();
+
+    // determine the size of the SVG element
+    let w = svg.node().getBoundingClientRect().width;
+    w = w - 40
+    let h = svg.node().getBoundingClientRect().height;
+    h = h - 25
+
+    const barWidth = w / jsonData.length;
+
+    const dataValues = jsonData.map((d) => LogToNum(d, selectedParam));
+
+
+    // set dynamic yscale
+    let yDomain;
+    if (selectedParam === "room") yDomain = [0.2, 0.8];
+    else if (selectedParam === "shape") yDomain = [0.2, 0.4];
+    else if (selectedParam === "fmi") yDomain = [0, 8];
+    else yDomain = [0, d3.max(dataValues, (d) => d) || 1];
+
+    // Create a YScale
+    let yScale = d3.scaleLinear()
+        .domain(yDomain)
+        .range([h, 0]);
+
+    // create chartGroup - translate the Bars to make room for axis
+    const chartGroup = svg.append('g')
+        .classed('chartGroup', true)
+        .attr('transform', `translate(30, 3)`);
+
+
+    // Set the gradient
+    svg.append("linearGradient")
+        .attr("id", "line-gradient")
+        .attr("gradientUnits", "userSpaceOnUse")
+        .attr("x1", 0)
+        .attr("y1", yScale(yDomain[0]))
+        .attr("x2", 0)
+        .attr("y2", yScale(yDomain[1]))
+        .selectAll("stop")
+        .data([
+            { offset: "0%", color: "green" },
+            { offset: "100%", color: "red" }
+        ])
+        .enter().append("stop")
+        .attr("offset", function (d) { return d.offset; })
+        .attr("stop-color", function (d) { return d.color; });
+
+
+
+    // Draw lines
+    chartGroup
+      .append('path')
+      .datum(dataValues)
+      .attr('fill', 'none')
+      .attr('stroke', 'url(#line-gradient)')
+      .attr('stroke-width', 1.5)
+      .attr('d', d3.line()
+        .x((d, i) => i * barWidth)
+        .y((d) => yScale(d))
+      )
+
+    let yAxis = d3.axisLeft(yScale);
+    chartGroup.append('g')
+      .classed('axis y', true)
+      .call(yAxis);
+
+  },[jsonData, selectedParam])
 
 
   return (
@@ -49,11 +122,14 @@ function Graph() {
         <h5 className="mb-3">Real-time Audio Parameter Visualizer</h5>
         <select value={selectedParam} onChange={(e) => setSelectedParam(e.target.value)}
           className="form-select w-auto mb-3">
-          <option value="shape">Shape</option>
-          <option value="room">Room</option>
-          <option value="lpf">LPF</option>
+          <option value="shape">Shape-Drums</option>
+          <option value="room">Room-Chords</option>
+          <option value="fmi">FMI-Melody</option>
 
         </select>
+        <svg width="100%" height="400px"
+          className="border border-primary rounded p-2">
+        </svg>
 
       </div>    
     </>
